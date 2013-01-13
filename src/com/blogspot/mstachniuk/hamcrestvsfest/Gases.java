51b736a1e0fe1468c2360e87f199d5fea7a66bab
Marcin Stachniuk <mstachniuk@gmail.com>
2013-01-13 13:45:06 +0100

    migrate to Maven

--- src/com/blogspot/mstachniuk/hamcrestvsfest/Gases.java
+++
@@ -1,5 +0,0 @@
-package com.blogspot.mstachniuk.hamcrestvsfest;
-
-public enum Gases {
-    OXYGEN, SODIUM, HYDROGEN, CARBON_DIOXIDE, NITROGEN;
-}
--- src/com/blogspot/mstachniuk/hamcrestvsfest/RotationDirection.java
+++
@@ -1,5 +0,0 @@
-package com.blogspot.mstachniuk.hamcrestvsfest;
-
-public enum RotationDirection {
-    LEFT, RIGHT, NONE, UNDEFINED
-}
